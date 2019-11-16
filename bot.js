25cf6b7abf15b20e9508cddc8e8cf507ba9eebb5
anclint01 <46342195+anclint01@users.noreply.github.com>
2019-11-16 23:35:34 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -854,7 +854,7 @@ bot.on("message", message => {
             var chapterLength;
             var fixedRequestedBook = requestedBook.replace(/ /g, "_").toLowerCase();
             var fulltitle;
-            console.log(numbersForNephi)
+            console.log(typeof numbersForNephi)
             if (typeof numbersForNephi != "string") {
                 var nephi = bom.books[0].numbers[numbersForNephi - 2].number;
             }
