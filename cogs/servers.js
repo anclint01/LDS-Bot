bbf042c31b396b9af8735275b7ee554c55edebd9
anclint01 <46342195+anclint01@users.noreply.github.com>
2021-04-08 19:45:28 -0600

    Update servers.js
    Made small Discord.js version change

--- cogs/servers.js
+++ cogs/servers.js
@@ -2,17 +2,14 @@ module.exports = {
     name: 'servers',
     description: 'Number of servers',
     execute(message, args, bot) {
-        if (message.author.id == 221285118608801802) {
-            var userColorPreference = 0xf2a93b;
-        } else {
-            var userColorPreference = 0x086587;
-        }
-	    message.channel.send({
-	        embed: {
-	            color: userColorPreference,
-	            title: "lds servers",
-	            description: "LDS-Bot has reached a total of **" + `${bot.guilds.size}` + "** servers"
-	        }
-	    });
+        if (message.author.id == 221285118608801802) var userColorPreference = 0xf2a93b;
+        else 					     var userColorPreference = 0x086587;        
+        message.channel.send({
+            embed: {
+	        color: userColorPreference,
+                title: "lds servers",
+	        description: "LDS-Bot has reached a total of **" + `${bot.guilds.cache.size}` + "** servers"
+            }
+	});
     },
 };
