d1d4d2aa624331ed63b759a46fd45f3b5dfc7025
anclint01 <46342195+anclint01@users.noreply.github.com>
2021-08-15 02:26:24 -0600

    Deleted in favor of new directory
    As part of the rewrite to typescript, this directory and its files are no longer exist

--- cogs/github.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'github',
-    description: 'Link to github repository',
-    execute(message, args) {
-        message.channel.send("https://github.com/anclint01/LDS-Bot");
-    },
-};
--- cogs/invite.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'invite',
-    description: 'Link to invite LDS-Bot',
-    execute(message, args) {
-		message.channel.send("https://discordapp.com/oauth2/authorize?permissions=93184&scope=bot&client_id=639271772818112564");
-    },
-};
