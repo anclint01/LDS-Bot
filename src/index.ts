b5aa921914ae0c49c097b3951d92b86d050f6dec
anclint01 <46342195+anclint01@users.noreply.github.com>
2021-09-09 23:09:22 -0600

    various updates
    Added slash commands, bible and delete button validation

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
-import { getClient, login } from './other/func';
+import { getClient, login } from './constants/func';
 import { messageHandler } from './handlers/message';
 import voftheday from "../votd.json";
 import { VOTD } from './handlers/VOTD';
 import fs from "fs";
 import schedule from "node-schedule";
 import Database from 'better-sqlite3';
+import CommandManager from './handlers/commands';
+import { Client, GuildMember, Message } from 'discord.js';
 
-const client = getClient();
-export const options = new Database('database/options');
+const client: Client = getClient();
+export const Manager: CommandManager = new CommandManager(client);
+Manager.loadCommands("./src/components/commands");
+export const commands = Manager.commands;
 
-
+export const options = new Database('./src/database/options');
 export const votd: VOTD = new VOTD();
-votd.setup();
 
 export const votdverse = fs.readFileSync('./votdverse.txt', 'utf8');
 export let randomVOTD: number;
@@ -29,6 +32,7 @@ schedule.scheduleJob('0 0 * * *', () => {
 async function main () {
   client
   .on('ready', async () => {
+    votd.setup();    
     let options_table;
     try {
       options_table = await options.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'options';").get();
@@ -42,11 +46,39 @@ async function main () {
         console.log(err)
       }      
     }
-    
     console.log("READY!");
   })
-  .on('messageCreate', messageHandler);
-  
+  .on('messageCreate', messageHandler)
+  .on('interactionCreate', async (i) => {
+    if (i.isButton()) {
+      if (i.customId.startsWith("delete")) {
+        if (!i.member) return;
+        let member = await (i.member as GuildMember).fetch();
+        let user_id = i.customId.split("_")[1];
+        if (member.permissions.has("MANAGE_MESSAGES") || member.user.id === user_id) {
+          let m = await (i.message as Message).fetch();
+          m.delete();
+        } else {
+          i.reply({content: "You cannot delete this reference as you were not the one to reference it OR you don't have MANAGE_MESSAGE perms", ephemeral: true})
+        }
+      }
+    }
+
+    if (!i.isCommand()) return;
+    const commandName = i.commandName;
+    if (commands.has(commandName.toLowerCase()) || commands.find((cmd: any) => cmd.aliases && cmd.aliases.includes(commandName.toLowerCase()))) {
+      const command = commands.get(commandName.toLowerCase()) || commands.find((cmmd: any) => cmmd.aliases && cmmd.aliases.includes(commandName.toLowerCase()))
+      
+      if (!command) return;
+
+      try {
+        command.execute_slash(i);
+      } catch (error) {
+        console.error(error);
+        await i.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+      }
+    } else return
+  });
   login();
 }
 
